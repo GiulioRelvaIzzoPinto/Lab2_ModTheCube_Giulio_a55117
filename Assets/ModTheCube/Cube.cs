27fcc82fe75fd18da155565520ed006b8b68456d
Giulio <giuliorelvaip@gmail.com>
2024-05-20 12:45:20 +0100

    Step 2: Modified the Cubes
    Changed its color to rgb, scaled it a little, and made it spin faster and on other axis!

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
+    public Material material;
+    public float hue;
     
     void Start()
     {
         transform.position = new Vector3(3, 4, 1);
-        transform.localScale = Vector3.one * 1.3f;
-        
-        Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        transform.localScale = Vector3.one * 3f;
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(100.0f * Time.deltaTime, 0.5f, 0.2f);
+        Material material = Renderer.material;
+        material.color = Color.HSVToRGB(0.33f, 1.0f, 0.7f);
+        CycleColors();
+    }
+
+    void CycleColors ()
+    {
+        hue += Time.deltaTime * 0.1f;
+        if (hue > 1.0f) hue = 0.0f;
+        Renderer.material.color = Color.HSVToRGB (hue, 1.0f, 1.0f);
     }
 }
